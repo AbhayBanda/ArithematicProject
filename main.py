8a8e9c3e94c77fe5cf673d602becaafe430858ee
Abhay Banda <bandaabhay@gmail.com>
2021-02-08 13:07:49 +0530

    Addition completed

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import addition as add
-import substraction as sub
-import division as div
-import multiplication as mul
+import addition
+import substraction
+import division
+import multiplication
 
 a, b = [int(x) for x in input("Enter two integers: ").split(' ')]
 
-printf("Addition :", add(a, b))
+print("Addition :", addition.add(a, b))
 printf("Substraction :", sub(a, b))
 printf("Division :", div(a, b))
 printf("Multiplication :", mul(a, b))
