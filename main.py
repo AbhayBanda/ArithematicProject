75db8bc90d5e6d3456231bf4569f963233f6fc21
Abhay Banda <bandaabhay@gmail.com>
2021-02-08 17:16:11 +0530

    Multiplication Completed

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import addition
 import substraction
 import division
-# import multiplication
+import multiplication
 
 a, b = [int(x) for x in input("Enter two integers: ").split(' ')]
 
 print("Addition :", addition.add(a, b))
 print("Substraction :", substraction.sub(a, b))
 print("Division :", division.div(a, b))
-# printf("Multiplication :", mul(a, b))
+print("Multiplication :", multiplication.mul(a, b))
