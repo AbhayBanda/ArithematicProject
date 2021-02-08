9ff8da3258a3551e58cc2c9d7d9ce0f3231bce80
Abhay Banda <bandaabhay@gmail.com>
2021-02-08 13:15:08 +0530

    Substraction Completed

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import multiplication
 a, b = [int(x) for x in input("Enter two integers: ").split(' ')]
 
 print("Addition :", addition.add(a, b))
-printf("Substraction :", sub(a, b))
+print("Substraction :", substraction.sub(a, b))
 printf("Division :", div(a, b))
 printf("Multiplication :", mul(a, b))
